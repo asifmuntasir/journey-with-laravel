70eab73191c0e8d4ca23e75fb07a719e4db77db7
asifmuntasir <asif.neub1702@gmail.com>
2025-07-30 10:54:40 +0800

    add all necessary fiels

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Journey With Laravel</title>
+</head>
+<body>
+    <h1>Welcome to the shit</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ut aut ex temporibus vel, optio illum culpa odio asperiores magnam velit.
+    </p>
+    <a href="#">Click Here for Get out from here!</a>
+</body>
+</html>
